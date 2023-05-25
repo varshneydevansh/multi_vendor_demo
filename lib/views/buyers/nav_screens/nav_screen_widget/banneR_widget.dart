fc1ede41676f2f7546d1d5b23d0c48dd49f1e255
देवांश वार्ष्णेय <varshney.devansh614@gmail.com>
2023-05-25 21:03:04 +0530

    now able to fetch images from cloud

--- lib/views/buyers/nav_screens/nav_screen_widget/banneR_widget.dart
+++ lib/views/buyers/nav_screens/nav_screen_widget/banneR_widget.dart
@@ -1,25 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class BannerWidget extends StatelessWidget {
-  const BannerWidget({super.key});
+class BannerWidget extends StatefulWidget {
+  @override
+  State<BannerWidget> createState() => _BannerWidgetState();
+}
+
+class _BannerWidgetState extends State<BannerWidget> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // first thing to do is to add a variable for cloud firestore for the banner
+  final List _bannerImage = [];
+
+  getBanners() {
+    return _firestore
+        .collection('Banners')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        setState(() {
+          _bannerImage.add(doc['image']);
+        });
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    //calling the getBanners() function to update the banner
+    getBanners();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.yellow.shade900,
-          boxShadow: [
-            BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                blurRadius: 10,
-                offset: Offset(0, 3))
-          ]),
-      child: Image.asset(
-        'assets/images/banner1.jpg',
-        fit: BoxFit.cover,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 200,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.yellow.shade900,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  blurRadius: 10,
+                  offset: Offset(0, 3))
+            ]),
+        child: PageView.builder(
+          itemCount: _bannerImage.length,
+          itemBuilder: (context, index) {
+          return Image.network(
+            _bannerImage[index],
+            fit: BoxFit.cover,
+          );
+        }),
       ),
     );
   }
